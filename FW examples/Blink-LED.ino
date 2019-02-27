1fd11cc0428471d60b07ccc0c2516f7d7f1f286a
petus <petus@users.noreply.github.com>
2019-02-27 17:27:45 +0100

    Update Blink-LED.ino

--- FW examples/Blink-LED.ino
+++ FW examples/Blink-LED.ino
@@ -10,16 +10,16 @@
  * Based on example from arduino.cc (Blink)
 */
 
-// the setup function runs once when you press reset or power the board
+// the setup function runs once when you disconnect DTR pin or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(D7, OUTPUT);
+  // initialize digital pin GPIO13 (D7) as an output.
+  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(D7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(D7, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
