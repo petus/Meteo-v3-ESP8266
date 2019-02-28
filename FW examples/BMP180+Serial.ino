8e93d4eb364716acc91b3dec76a1d8cd947a9bbc
petus <petus@users.noreply.github.com>
2019-02-28 22:09:46 +0100

    Create BMP180+Serial.ino

---
+++ FW examples/BMP180+Serial.ino
@@ -0,0 +1,41 @@
+/*
+ * Meteo-v3 
+ * BMP180
+ * BMP180 module is connected to I2C connector. Temperature and 
+ * pressure are shown on Serial Monitor (9600)
+ * 
+ * made by chiptron.cz (2019)
+ * 
+ * czech website www.chiptron.cz
+ * english website www.time4ee.com
+ * 
+ * Adafruit BMP085 library (BMP180 is supported)
+*/
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+  
+// the setup function runs once when you disconnect DTR pin or power the board
+void setup() {
+  
+  Serial.begin(9600); // initialize Serial monitor with 9600
+  bmp.begin();  
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  int temperature = bmp.readTemperature();  // read temperature
+  int pressure = bmp.readPressure();  // read pressure
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+  Serial.print("Pressure: ");
+  Serial.print(pressure);
+  Serial.println(" Pa");
+  delay(1000);  // wait one second
+  
+}
